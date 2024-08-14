9fe6462e0d07899a0ece516235866c3665ec0144
Akshintala Sri Harsha <akshintalasriharsha@yahoo.com>
2024-08-14 14:31:33 +0530

    Basic topics (Execution Context, Hoisting, Lexical Environment, Scope chain)

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+var x = 10;
+function getName() {
+  console.log("Namaste Javascript");
+}
+
+getName();
+console.log(x);
+console.log(getName);
---
+++ script2.js
@@ -0,0 +1,13 @@
+var x = 10;
+a();
+b();
+console.log(x);
+function a() {
+  var x = 100;
+  console.log(x);
+}
+
+function b() {
+  var x = 1000;
+  console.log(x);
+}
