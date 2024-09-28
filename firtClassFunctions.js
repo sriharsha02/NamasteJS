d97c1d83c79c761d46873dda7aed42f28344d61a
Akshintala Sri Harsha <akshintalasriharsha@yahoo.com>
2024-09-28 22:10:27 +0530

    functions
    anonymous, name function expression, parameters, arguments

--- firtClassFunctions.js
+++ firtClassFunctions.js
@@ -1,10 +1,40 @@
-//Function statement
+//Function statement also known as Function Declaration
 
 function a() {
   console.log("a is called");
 }
 
+a();
 //Function Expression
-var a = function () {
-  console.log("a is called");
+var b = function () {
+  console.log("b is called");
 };
+b();
+
+//Anonymous Functions
+
+// function(){
+
+// }
+
+//Named Function Expression
+// var factorial = function fact() {
+//   console.log("fact is called");
+// };
+
+// fact();
+
+var factorial = function fact() {
+  console.log(fact);
+};
+factorial();
+//Difference between parameters and Arguments
+
+function add(a, b) {
+  return a + b;
+}
+add(2, 3);
+
+//First class functions
+
+//Arrow Functions
