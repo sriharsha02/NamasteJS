748ea88280561b0558179b85e93580fda32060de
Akshintala Sri Harsha <akshintalasriharsha@yahoo.com>
2024-09-30 22:54:43 +0530

    functional programming

---
+++ higherOrderFunctions.js
@@ -0,0 +1,43 @@
+const radius = [2, 4, 6, 8];
+
+const calculateArea = function (radius) {
+  let output = [];
+  for (let i = 0; i < radius.length; i++) {
+    output.push(Math.PI * radius[i] * radius[i]);
+  }
+  return output;
+};
+console.log(calculateArea(radius));
+
+const calculateCircumference = function (radius) {
+  let output = [];
+  for (let i = 0; i < radius.length; i++) {
+    output.push(2 * Math.PI * radius[i]);
+  }
+  return output;
+};
+console.log(calculateCircumference(radius));
+
+const calculateDiameter = function (radius) {
+  let output = [];
+  for (let i = 0; i < radius.length; i++) {
+    output.push(2 * radius[i]);
+  }
+  return output;
+};
+console.log(calculateDiameter(radius));
+
+//Optimizing above code : Functional Programming
+
+const calculate = function (radius, logic) {
+  let output = [];
+  for (let i = 0; i < radius.length; i++) {
+    output.push(logic(radius[i]));
+  }
+  return output;
+};
+const area = function (radius) {
+  return Math.PI * radius * radius;
+};
+
+console.log(calculate(radius, area));
