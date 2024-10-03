3721fe139f6612ba3ae3b42bd48294839a13f19b
Akshintala Sri Harsha <akshintalasriharsha@yahoo.com>
2024-10-03 16:34:06 +0530

    reduce
    Higher order function

---
+++ reduce.js
@@ -0,0 +1,46 @@
+const arr = [5, 1, 3, 2, 6];
+
+//sum
+
+function findSum(array) {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum = sum + array[i];
+  }
+  return sum;
+}
+
+console.log(findSum(arr));
+
+//using reduce
+
+const output = arr.reduce(function (acc, curr) {
+  acc = acc + curr;
+  return acc;
+}, 0);
+console.log(output);
+
+//max
+
+function findMax(array) {
+  let max = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  return max;
+}
+
+console.log(findMax(arr));
+
+//using reduce
+
+const maxOutput = arr.reduce(function (acc, curr) {
+  if (curr > acc) {
+    acc = curr;
+  }
+  return acc;
+}, 0);
+
+console.log(maxOutput);
