72de956167b06164e47080c69224111107c11fd1
Akshintala Sri Harsha <akshintalasriharsha@yahoo.com>
2024-10-03 16:59:16 +0530

    added examples

--- filter.js
+++ filter.js
@@ -19,5 +19,20 @@ console.log(even);
 function isEven(x) {
   return x > 4;
 }
-const greaterThanFour = arr.filter(greaterThanFour);
+const greaterThanFour = arr.filter((num) => num > 4);
 console.log(greaterThanFour);
+
+//example
+
+const users = [
+  { firstName: "akshay", lastName: "Saini", age: 26 },
+  { firstName: "donald", lastName: "Trump", age: 70 },
+  { firstName: "Elon", lastName: "Musk", age: 50 },
+  { firstName: "Sri Harsha", lastName: "Akshintala", age: 28 },
+];
+
+const aboveFifty = users.filter(function (user) {
+  return user.age > 50;
+});
+
+console.log(aboveFifty);
--- reduce.js
+++ reduce.js
@@ -44,3 +44,23 @@ const maxOutput = arr.reduce(function (acc, curr) {
 }, 0);
 
 console.log(maxOutput);
+
+//example
+
+const users = [
+  { firstName: "akshay", lastName: "Saini", age: 26 },
+  { firstName: "donald", lastName: "Trump", age: 70 },
+  { firstName: "Elon", lastName: "Musk", age: 50 },
+  { firstName: "Sri Harsha", lastName: "Akshintala", age: 28 },
+];
+
+const sameAgeNumber = users.reduce(function (acc, curr) {
+  if (acc[curr.age]) {
+    acc[curr.age] = ++acc[curr.age];
+  } else {
+    acc[curr.age] = 1;
+  }
+  return acc;
+}, {});
+
+console.log(sameAgeNumber);
